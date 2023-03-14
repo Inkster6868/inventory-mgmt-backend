70dab838b7822fc5a93c60dfbb3f2024d1e5270e
Tushar <tushardua6868@gmail.com>
2023-03-14 19:05:36 +0530

    changed cors link

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());  //converts the incoming body data to .json format
 app.use(cookieParser());
 app.use(express.urlencoded({extended:false})); //??
 app.use(cors({
-    origin:["http://localhost:3000", "https://streamlinemgmtt.vercel.app"], 
+    origin:["http://localhost:3000", "https://streamlinemgmt.vercel.app"], 
     credentials:true                   
 }));
 
